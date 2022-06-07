82119d8fd048f1217f27032550c5d9c17dac120c
Jenny Hunter <jenny@researchlead.com>
2022-06-07 11:01:37 -0700

    Add basic navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header>
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Navbar.Brand href="#home">The Dam Lodge</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#menu">Menu</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
       </header>
+      <Container>
+        
+      </Container>
     </div>
   );
 }
