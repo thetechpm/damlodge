e87282dcd1b94fbe1b0f9bf41e2471002fed2e85
Jenny Hunter <jenny@researchlead.com>
2022-06-07 22:43:00 -0700

    Remove carousel and add header and menu

--- src/App.js
+++ src/App.js
@@ -1,42 +1,58 @@
-import {  Carousel, Container, Nav, Navbar } from 'react-bootstrap';
-import './App.css';
+// Bootstrap components
+import {  Carousel, Col, Container} from 'react-bootstrap';
+
+// Libraries
+import { Document, Page, pdfjs } from 'react-pdf';
+import { useState } from 'react';
+
+
+// Stylesheet
+import './App.scss';
+
+const options = {
+  standardFontDataUrl: 'standard_fonts/',
+};
 
 function App() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="App">
       <header>
-        <Navbar bg="light" variant="light">
-          <Container>
-            <Navbar.Brand href="#home">The Dam Lodge</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#menu">Menu</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
+        <Col xs={12} className='header-container'>
+          <div className='background-image' style={{backgroundImage: `url(${"./images/view-landscape-long.jpg"})`}} />
+          <div className='header-body-container'>
+            <h1>The Dam Lodge</h1>
+          </div>
+        </Col>
       </header>
       <Container>
-        <h1 id="home">The Dam Lodge</h1>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="./images/aerial-landscape.jpg"
-              alt="Aerial View"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="./images/view-landscape.jpg"
-              alt="Aerial View"
-            />
-          </Carousel.Item>
-        </Carousel>
-        <p>The Dam Lodge overlooks Riffe Lake and the Mossyrock Dam. In about five minutes you can reach the idyllic Swofford Pond, a popular fishing location for the area, and in a little over ten minutes, you can reach the Mossyrock Park, a lakeside park with 152 campsites & seasonal boat launch, swimming, general store, & concessions.</p>
+        
+        <p className="lead text-center mt-5">The Dam Lodge overlooks Riffe Lake and the Mossyrock Dam. In about five minutes you can reach the idyllic Swofford Pond, a popular fishing location for the area, and in a little over ten minutes, you can reach the Mossyrock Park, a lakeside park with 152 campsites & seasonal boat launch, swimming, general store, & concessions.</p>
         
         <h2 id="menu">Menu</h2>
+        <Container>
+          <Document file="./Dam Lodge Menu.pdf" onLoadSuccess={onDocumentLoadSuccess} options={{
+            standardFontDataUrl: `https://unpkg.com/pdfjs-dist@${pdfjs.version}/standard_fonts`,
+          }}>
+            <Page pageNumber={1} />
+            <Page pageNumber={2} />
+            <Page pageNumber={3} />
+          </Document>
+        </Container>
       </Container>
+      <footer>
+        <Container className="my-3 text-center">
+          <p className="small">©{currentYear} Jenny Hunter</p>
+        </Container>
+      </footer>
     </div>
   );
 }
