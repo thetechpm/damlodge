d6f63dea42dd5846532ae66eee8fd0cf15f7575d
Jenny Hunter <jenny@researchlead.com>
2023-09-02 15:31:00 -0700

    Addresed unusued var warning

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,22 +3,16 @@ import {  Button, Container} from 'react-bootstrap';
 
 // Libraries
 import { Document, Page, pdfjs } from 'react-pdf';
-import { useState } from 'react';
-
 
 function Menu() {
-  const [numPages, setNumPages] = useState(null);
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
 
   return (
     <div className="Menu">
         <h2 className="mt-5" id="menu">Menu</h2>
         <Container>
         <Button size="sm" variant="secondary" href="./Dam Lodge Menu.pdf">View menu directly</Button>
-          <Document file="./Dam Lodge Menu.pdf" onLoadSuccess={onDocumentLoadSuccess} options={{
+          <Document file="./Dam Lodge Menu.pdf" options={{
             standardFontDataUrl: `https://unpkg.com/pdfjs-dist@${pdfjs.version}/standard_fonts`,
           }}>
             <Page pageNumber={1} />
